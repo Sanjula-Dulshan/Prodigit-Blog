de2864d645264151cb78ef4119e9eaa97d370e68
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-02-29 01:11:38 +0530

    feat: connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
 
@@ -9,5 +10,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
 
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
