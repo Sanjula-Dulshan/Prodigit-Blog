766d436fdba45fef7ed852f5b8ebed2a34076c92
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-02-29 01:02:01 +0530

    init api

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
