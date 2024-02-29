9ba81c50fcb993bf0108a57e7b02bbb8ca394f45
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-02-29 09:53:25 +0530

    init post, category, comment models

---
+++ api/models/post.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: "https://placehold.co/600x400.png",
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  categories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
