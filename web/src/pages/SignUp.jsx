677eb2d972b6e8a6c4cf64071b86d9bdd799d734
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-04-08 23:27:14 +0530

    integrate sign up ui with api

--- web/src/pages/SignUp.jsx
+++ web/src/pages/SignUp.jsx
@@ -1,7 +1,36 @@
 import { Link } from "react-router-dom";
 import { Button, Label, Select, TextInput } from "flowbite-react";
+import { useRef } from "react";
+import axios from "axios";
 
 export default function SignUp() {
+  const fullNameRef = useRef();
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+  const genderRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      fullName: fullNameRef.current.value,
+      username: usernameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      confirmPassword: confirmPasswordRef.current.value,
+      gender: genderRef.current.value,
+    };
+    console.log(data);
+
+    try {
+      const res = await axios.post("/api/auth/signup", data);
+      console.log("res", res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -22,8 +51,24 @@ export default function SignUp() {
         <div className="flex-1">
           <form className="flex flex-col gap-4">
             <div>
+              <Label value="Full name" />
+              <TextInput
+                type="text"
+                placeholder="Full name"
+                id="fullName"
+                required
+                ref={fullNameRef}
+              />
+            </div>
+            <div>
               <Label value="Your username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                required
+                ref={usernameRef}
+              />
             </div>
             <div>
               <Label value="Your email" />
@@ -31,11 +76,19 @@ export default function SignUp() {
                 type="email"
                 placeholder="name@company.com"
                 id="email"
+                required
+                ref={emailRef}
               />
             </div>
             <div>
               <Label value="Your password" />
-              <TextInput type="password" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                required
+                ref={passwordRef}
+              />
             </div>
             <div>
               <Label value="Confirm password" />
@@ -43,18 +96,27 @@ export default function SignUp() {
                 type="password"
                 placeholder="Confirm Password"
                 id="confirmPassword"
+                required
+                ref={confirmPasswordRef}
               />
             </div>
 
             <div>
               <Label value="Gender" />
-              <Select id="gender">
-                <option>Male</option>
-                <option>Female</option>
+              <Select id="gender" ref={genderRef} required>
+                <option value="" disabled>
+                  Select Gender
+                </option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
               </Select>
             </div>
 
-            <Button gradientDuoTone="greenToBlue" type="submit">
+            <Button
+              gradientDuoTone="greenToBlue"
+              type="submit"
+              onClick={handleSubmit}
+            >
               Sign Up
             </Button>
           </form>
